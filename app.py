e0bc837a12c084717e6d3804cfc33e7c9aef8981
Manuman76 <mlegault18@gmail.com>
2024-10-04 17:16:27 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import pymongo
+
+
+def create_app():
+    app = Flask(__name__)
+
+    myclient = pymongo.MongoClient("mongodb://192.168.1.128:32768/")
+    mydb = myclient["cv-manager"]
+    mycol = mydb["employees"]
+
+    myquery = { "manager": "manuel.legault@alithya.com" }
+    mydoc = mycol.find(myquery)
+
+    @app.route('/')
+    def index():
+        return render_template('my-team.html', mydoc=mydoc)
+
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
